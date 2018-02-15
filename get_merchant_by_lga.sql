93c332ed427ddbc1bfc8eb8193d4422fa856d862
Opesemowo Babatunde <tunexlevel8@gmail.com>
2018-02-15 14:42:50 +0100

    Create get_merchant_by_lga.sql

---
+++ get_merchant_by_lga.sql
@@ -0,0 +1,4 @@
+select merchant_code, merchant_name, state_code, lga count(lga) as lga_count 
+from merchants
+inner join terminal
+on merchants.merchant_id = terminal.merchant_id  group by lga, merchant_code, merchant_name, state_code
