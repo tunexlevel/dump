550d2d2687f5a707083ec626520503119f4f1a21
Opesemowo Babatunde <tunexlevel8@gmail.com>
2018-02-23 11:26:37 +0100

    Update symonJsAttached.js

--- symonJsAttached.js
+++ symonJsAttached.js
@@ -12,3 +12,18 @@ else if (path === "/query/pos/sbu") {
     response.write(JSON.stringify({status: 200, list: sbu}));
     return response.end();
 }
+
+else if (path === "/query/pos/sbu/transaction") {
+    if (user.level < 5) {
+        response.write('{"status": 403, "message": "Access denied. You MUST login to query terminals."}');
+        return response.end();
+    }
+    
+    let list = [];
+    let sql, options = [];
+
+    sql = "select sbu, sum(transaction_amount) as t_amount from pos_transaction inner join merchants on pos_transaction.terminal_id = merchants.terminal_id where sbu = $1 and timestamp >= $2 and timestamp <= $3 group by sbu";
+    let sbu = yield db.query(sql, options);
+    response.write(JSON.stringify({status: 200, list: sbu}));
+    return response.end();
+}
